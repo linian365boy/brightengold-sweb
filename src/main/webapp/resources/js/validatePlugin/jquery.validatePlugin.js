be6f18fe429361b4dcf6541f3559e8404e4c421d
nian <linian365boy@foxmail.com>
2014-04-24 18:26:24 +0800

    修改bug

--- src/main/webapp/resources/js/validatePlugin/jquery.validatePlugin.js
+++ src/main/webapp/resources/js/validatePlugin/jquery.validatePlugin.js
@@ -1,5 +1,5 @@
 //图片文件验证
  jQuery.validator.addMethod("isImgFile", function(value, element) {
-	 var picture = /.(jpg|jpeg|bmp|gif)$/;
+	 var picture = /.(jpg|jpeg|bmp|gif|png)$/;
      return (value==''||picture.exec(value.toLowerCase()))?true:false;       
- }, "请上传jpg|jpeg|bmp|gif图片格式文件");
+ }, "请上传jpg|jpeg|bmp|gif|png图片格式文件");
